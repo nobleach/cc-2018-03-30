5987ad4748769ba4d5578530e4b55ec843eb653c
Jim Wharton <jimwharton@overstock.com>
2018-03-30 20:29:53 -0600

    initial commit

---
+++ TapeMachine/Program.fs
@@ -0,0 +1,30 @@
+﻿// Learn more about F# at http://fsharp.org
+
+module TapeMachine
+open System
+
+type TapeState =
+    | Playing
+    | Paused
+    | Stopped
+
+type Events =
+    | PressPlay
+    | PressStop
+    | PressPause
+
+let act = function
+    | Stopped, PressPlay -> Playing
+    | (Playing | Paused), PressStop -> Stopped
+    | Playing, PressPause -> Paused
+    | Stopped, PressPause -> Stopped
+    | Paused, PressPause -> Playing
+
+[<EntryPoint>]
+let main argv =
+    act (Stopped, PressPlay) |> printfn "Stopped, Press Play: %A"
+    act (Playing, PressStop) |> printfn "Playing, PressStop: %A"
+    act (Playing, PressPause) |> printfn "Playing, PressPause: %A"
+    act (Paused, PressPause) |> printfn "Paused, PressPause: %A"
+    act (Stopped, PressPause) |> printfn "Stopped, PressPause: %A"
+    0 // return an integer exit code
